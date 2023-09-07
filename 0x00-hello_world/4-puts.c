a79e96c10eb6da46d0a1d4e4a8621a93d43ca206
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-07 11:52:54 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,3 @@
-#include <stdio.h>
-/**
- * main - Entry point of the program
- *
- * Description: Prints the specified string with double quotes and returns 0.
- *
- * Return: Always 0 (success)
- */
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
