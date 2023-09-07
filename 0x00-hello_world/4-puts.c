02aa859eb8ce9a3d29b3953ee86721bcd4077f92
Daniel Amoako Kodua <officialjwise20@gmail.com>
2023-09-07 04:43:11 -0700

    4-Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Description: Prints the specified string and returns 0.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
