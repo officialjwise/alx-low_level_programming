830d099c0dfd8db4ccf0195c788c68931ec182c9
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-07 12:46:04 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
 /**
  * main - Entry point of the program
- * Return: 1 (success)
+ *
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-        fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+    fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+    return (1);
 }
