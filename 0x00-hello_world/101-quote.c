501cc4aefe8f0eff0d3e6251edd3969bfdc6427f
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-07 12:19:12 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,14 @@
 /**
  * main - Entry point of the program
  *
+ * Description: Prints the specified message to the standard error.
+ *
  * Return: Always 1 (error)
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *message = "and that piece of art is useful\" - Dora Korpar, "
+			"2015-10-19\n";
 ssize_t len = 59;
 if (write(2, message, len) != len)
 return (1);
