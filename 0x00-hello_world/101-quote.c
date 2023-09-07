e033718e2ef271e889780f73376fbb94936ab035
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-07 12:16:20 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,9 +10,9 @@
  */
 int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = 59;
-    if (write(2, message, len) != len)
-        return (1);
-    return (EXIT_SUCCESS);
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t len = 59;
+if (write(2, message, len) != len)
+return (1);
+return (EXIT_SUCCESS);
 }
