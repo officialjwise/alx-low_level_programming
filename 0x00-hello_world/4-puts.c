5dc498f178519f32e9eaa9bdd65729d5af76dbfb
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-07 11:53:54 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,21 @@
+/*
+ * 4-puts.c
+ *
+ * A program that prints the string "Programming is like building
+ * a multilingual puzzle" to the standard output stream.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the specified string and returns 0.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+  puts("Programming is like building a multilingual puzzle");
+  return (0);
 }
