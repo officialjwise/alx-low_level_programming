19d6fe658e300dd420a580b252520b7b9d549485
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-09-09 09:33:24 +0000

    7-Print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter = 'z';
+    while (letter >= 'a')
+    {
+        putchar(letter);
+        letter--;
+    }
+    putchar('\n');
+    return (0);
+}
