6272fd0ef93d7005c4001ec415321b6fb71763b3
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-09-09 01:16:59 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+for (digit = 0; digit <= 9; digit++)
+{
+putchar(digit + '0');
+}
+putchar('\n');
+return (0);
+}
