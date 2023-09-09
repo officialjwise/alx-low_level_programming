589bd5c8a9072fb06b763665533c2f7f22f9577c
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-09-09 09:27:58 +0000

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,11 @@
-/* Betty style C program to print all single digit numbers of base 10 */
 #include <stdio.h>
-int main() {
-  /* Declare a loop counter variable */
-  int i;
-  /* Iterate from 0 to 9 */
-  for (i = 0; i <= 9; i++) {
-    /* Print the ASCII code of the digit corresponding to i */
-    putchar(i + '0');
-  }
-  /* Print a newline character */
-  putchar('\n');
-  /* Return 0 to indicate successful completion */
-  return 0;
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+putchar(48); /* 48 is the ASCII code for '0' */
+putchar(10); /* 10 is the ASCII code for a newline character */
+return (0);
 }
