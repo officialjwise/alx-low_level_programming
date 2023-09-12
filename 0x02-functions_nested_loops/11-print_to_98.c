88044eae10d827d3de0ea6632c8aa8836be59034
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-12 20:25:03 +0000

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,31 +2,22 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98
- * @n: The starting number
+ * print_to_98 - writes the character c to stdout
+ * @n: The character to test
+ *
  */
 void print_to_98(int n)
 {
-int i;
-
-if (n <= 98)
-{
-for (i = n; i <= 98; i++)
-{
-printf("%d", i);
-if (i != 98)
-printf(", ");
-}
-}
-else
-{
-for (i = n; i >= 98; i--)
-{
-printf("%d", i);
-if (i != 98)
-printf(", ");
-}
-}
-
-printf("\n");
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	printf("98");
+	putchar('\n')
 }
