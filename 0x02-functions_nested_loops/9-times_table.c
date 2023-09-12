097f2dc594e60fceb7a6dc8183f932d9015b4db3
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-12 20:05:56 +0000

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,40 +1,40 @@
-User
 #include "main.h"
 
 /**
-* times_table - Prints the 9 times table starting with 0
-*/
+ * times_table - prints the 9 times table starting with 0
+ * Return: 0 if success
+ */
+
 void times_table(void)
 {
-int row, column, result;
+	int row, column, result;
 
-for (row = 0; row <= 9; row++)
-{
-for (column = 0; column <= 9; column++)
-{
-result = row * column;
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			result = row * column;
+			if (column == 0)
+			{
+				_putchar('0' + result);
+			}
+			else if (result < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
 
-if (column == 0)
-{
-_putchar('0');
-_putchar(',');
-} else
-{
-_putchar(' ');
-if (result < 10)
-{
-_putchar(' ');
-} else
-{
-_putchar((result / 10) + '0');
-}
-_putchar((result % 10) + '0');
-if (column < 9)
-{
-_putchar(',');
-}
-}
-}
-_putchar('\n');
-}
+			if (column < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
