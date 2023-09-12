90c0807ad90f3e3f8b3136cc725819f884a26e52
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-12 19:09:25 +0000

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,11 +9,11 @@ for (hours = 0; hours < 24; hours++)
 {
 for (minutes = 0; minutes < 60; minutes++)
 {
-_putchar('0' + (hours / 10));  
-_putchar('0' + (hours % 10));  
+_putchar('0' + (hours / 10));
+_putchar('0' + (hours % 10));
 _putchar(':');
-_putchar('0' + (minutes / 10)); 
-_putchar('0' + (minutes % 10)); 
+_putchar('0' + (minutes / 10));
+_putchar('0' + (minutes % 10));
 _putchar('\n');
 }
 }
