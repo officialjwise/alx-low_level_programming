9bfb045ca242b5f0dd649e342f1157fa5cac6fe8
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-12 18:16:32 +0000

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,5 @@
-#include "main.h"
-/**
- * print_alphabet - Prints the alphabet in lowercase followed by a new line
- */
-void print_alphabet(void)
-{
-char letter = 'a';
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
-}
-_putchar('\n');
-}
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void print_alphabet(void);
+#endif /* MAIN_H */
