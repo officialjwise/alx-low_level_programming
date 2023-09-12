a98a9176a1c48572832b3f18b06153486bc20978
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-12 19:49:21 +0000

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,31 +7,24 @@ void times_table(void)
 {
 int row, column, result;
 
-for (row = 0; row <= 9; row++)
-{
-for (column = 0; column <= 9; column++)
-{
+for (row = 0; row <= 9; row++) {
+for (column = 0; column <= 9; column++) {
 result = row * column;
 
-if (column == 0)
-{
+if (column == 0) {
 _putchar('0');
 _putchar(',');
+} else {
 _putchar(' ');
-}
-else
-{
+if (result < 10) {
 _putchar(' ');
-if (result < 10)
-_putchar(' ');
-else
+} else {
 _putchar((result / 10) + '0');
+}
 
 _putchar((result % 10) + '0');
-if (column < 9)
-{
+if (column < 9) {
 _putchar(',');
-_putchar(' ');
 }
 }
 }
