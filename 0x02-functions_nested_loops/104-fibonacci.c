8b70d75acccecb2601319df26dad59152b17dab0
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-12 21:42:32 +0000

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,45 +2,45 @@
 #include <gmp.h>
 
 /**
- * main - main block
- * Description: first 98 Fibonacci numbers, starting with 1 and 2
- * followed by a new line.
- * Return: 0
- */
+* main - main block
+* Description: first 98 Fibonacci numbers, starting with 1 and 2
+* followed by a new line.
+* Return: 0
+*/
 int main(void)
 {
-	int i = 0;
-	mpz_t a, b, next;
+int i = 0;
+mpz_t a, b, next;
 
-	// Initialize the mpz_t variables
-	mpz_init_set_ui(a, 0); // a = 0
-	mpz_init_set_ui(b, 1); // b = 1
-	mpz_init(next); // next = 0
+// Initialize the mpz_t variables
+mpz_init_set_ui(a, 0); // a = 0
+mpz_init_set_ui(b, 1); // b = 1
+mpz_init(next); // next = 0
 
-	while (i < 98)
-	{
-		// next = a + b
-		mpz_add(next, a, b);
+while (i < 98)
+{
+// next = a + b
+mpz_add(next, a, b);
 
-		// a = b
-		mpz_set(a, b);
+// a = b
+mpz_set(a, b);
 
-		// b = next
-		mpz_set(b, next);
+// b = next
+mpz_set(b, next);
 
-		// Print next
-		gmp_printf("%Zd", next);
+// Print next
+gmp_printf("%Zd", next);
 
-		if (i < 97)
-			printf(", ");
-		i++;
-	}
-	putchar('\n');
+if (i < 97)
+printf(", ");
+i++;
+}
+putchar('\n');
 
-	// Clear the mpz_t variables
-	mpz_clear(a);
-	mpz_clear(b);
-	mpz_clear(next);
+// Clear the mpz_t variables
+mpz_clear(a);
+mpz_clear(b);
+mpz_clear(next);
 
-	return (0);
+return (0);
 }
