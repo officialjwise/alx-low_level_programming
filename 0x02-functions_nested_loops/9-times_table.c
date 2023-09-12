de804c49fe145f1245b08dca6db865f4ceabe34b
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-12 19:24:00 +0000

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,24 +13,17 @@ for (column = 0; column <= 9; column++)
 {
 result = row * column;
 
-if (column == 0)
-_putchar('0');
-else if (result < 10)
+if (column > 0)
+_putchar(','); /* Print a comma before the number */
+
+/* Print the result with proper formatting */
+if (result < 10)
 _putchar(' '); /* Add a space for single-digit results */
 else
 _putchar((result / 10) + '0'); /* Print the tens digit */
 
 _putchar((result % 10) + '0'); /* Print the ones digit */
-
-if (column < 9)
-{
-_putchar(','); /* Print a comma */
-_putchar(' '); /* Print a space */
 }
-else
-{
 _putchar('\n'); /* Print a newline at the end of each row */
-}
-}
 }
 }
