01a1067d68ff6be39a6e5b8c23c726f4544f02c9
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-12 21:07:12 +0000

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 
 void print_num(int n);
 
@@ -22,6 +21,11 @@ void print_to_98(int n)
 		print_num(n);
 	printf("\n");
 }
+
+/**
+ * print_num - helper function to print numbers
+ * @n: number to print
+ */
 void print_num(int n)
 {
 	printf("%d%s", n, (n != 98) ? ", " : "");
