571f512ac6f7103e64e6ac1aebdec4863ae2bea3
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-09-12 18:40:06 +0000

    Print the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+/**
+ * print_last_digit - Prints and returns the last digit of an integer
+ * @n: The integer to extract the last digit from
+ *
+ * Return: The value of the last digit
+ */
+int print_last_digit(int n)
+{
+int last_digit;
+
+if (n < 0)
+last_digit = -1 * (n % 10);
+else
+last_digit = n % 10;
+
+_putchar('0' + last_digit);
+
+return last_digit;
+}
