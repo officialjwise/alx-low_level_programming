60634be4830732d6eefccb1ae81cafe94e578dfc
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-09-12 18:36:31 +0000

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: The integer to compute the absolute value for
+ *
+ * Return: The absolute value of n
+ */
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
