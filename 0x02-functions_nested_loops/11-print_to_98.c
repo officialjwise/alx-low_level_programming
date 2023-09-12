aa3e68a862fbf22db44845a21574bbab8a935abb
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-09-12 20:19:43 +0000

    Print 11 to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - writes the character c to stdout
+ * @n: The character to test
+ *
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+
+	printf("98");
+	putchar('\n');
+
+
+}
