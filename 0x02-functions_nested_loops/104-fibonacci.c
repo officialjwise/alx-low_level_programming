a26eb44892d7afa783a693f027762cf2d38c30f3
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-12 21:25:09 +0000

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,20 +1,39 @@
+#include <stdio.h>
+
 /**
-* main - printing first 98 fibonacci sequence
-* Return: 0
-*/
+ * print_large_number - Prints a large number that may exceed 1 billion.
+ * @high: The high part of the number (divided by 1 billion).
+ * @low: The low part of the number (modulo 1 billion).
+ */
+void print_large_number(unsigned long high, unsigned long low)
+{
+	if (high > 0)
+		printf("%lu%09lu", high, low);
+	else
+		printf("%lu", low);
+}
+
+/**
+ * main - printing first 98 fibonacci sequence
+ * Return: 0
+ */
 int main(void)
 {
-unsigned long int current = 1;
-unsigned long int next = 2;
-int i;
-printf("%lu", current);
-for (i = 2; i < 98; i++)
-{
-printf(", ");
-print_large_number(0, next);
-next += current;
-current = next - current;
-}
-printf("\n");
-return (0);
+	unsigned long int current = 1;
+	unsigned long int next = 2;
+	int i;
+
+	printf("%lu, ", current);
+
+	for (i = 2; i < 98; i++)
+	{
+		print_large_number(0, next);
+		if (i < 97)
+			printf(", ");
+		next += current;
+		current = next - current;
+	}
+	printf("\n");
+
+	return (0);
 }
