bab8d642d3585e4e4eff9b5e427a27b43b5d1ed1
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-12 20:50:52 +0000

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
-* print_to_98 - Prints all natural numbers from n to 98
-* @n: The starting number
-*/
-void print_to_98(int n) {
-for (int i = n; i <= 98; i++) {
-putchar(i + '0');
-if (i < 98) {
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_to_98(0);
+    print_to_98(98);
+    print_to_98(111);
+    print_to_98(81);
+    print_to_98(-10);
+    return (0);
 }
