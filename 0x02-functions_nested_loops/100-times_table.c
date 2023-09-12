39dcf9573b7c0fd101f616b372bf72a9963b5476
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-12 21:11:52 +0000

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,51 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the 9 times table starting with 0
- * @n: times table
- * Return: 0 if success
- */
+* print_times_table - prints the 9 times table starting with 0
+* @n: times table
+* Return: 0 if success
+*/
 
 void print_times_table(int n)
 {
-	int row, column, result;
-	
-	if (n < 0 || n > 15)
-		return;
+int row, column, result;
+if (n < 0 || n > 15)
+return;
 
-	for (row = 0; row <= n; row++)
-	{
-		for (column = 0; column <= n; column++)
-		{
-			result = row * column;
-			if (column == 0)
-			{
-				_putchar('0' + result);
-			}
-			else if (result < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + result);
-			}
-			else if (result < 100)
-			{
-				_putchar(' ');
-				_putchar('0' + result / 10);
-				_putchar('0' + result % 10);
-			}
-			else
-			{
-				_putchar('0' + result / 100);
-				_putchar('0' + (result - 100) / 10);
-				_putchar('0' + result % 10);
-			}
+for (row = 0; row <= n; row++)
+{
+for (column = 0; column <= n; column++)
+{
+result = row * column;
+if (column == 0)
+{
+_putchar('0' + result);
+}
+else if (result < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + result);
+}
+else if (result < 100)
+{
+_putchar(' ');
+_putchar('0' + result / 10);
+_putchar('0' + result % 10);
+}
+else
+{
+_putchar('0' + result / 100);
+_putchar('0' + (result - 100) / 10);
+_putchar('0' + result % 10);
+}
 
-			if (column < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
+if (column < n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
