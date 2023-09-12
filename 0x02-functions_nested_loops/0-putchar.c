d8a230cbb1e2d9bacb50b2823b7629dc8d25587c
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-09-12 17:56:11 +0000

    Putchar question

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char str[] = "_putchar\n";
+    int i;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        _putchar(str[i]);
+    }
+    return (0);
+}
