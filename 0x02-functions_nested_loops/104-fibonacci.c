b19c67b3a31ffa4812dde0041cfc2a946495927e
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-12 21:23:51 +0000

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,23 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * print_fibonacci - Prints the first 98 Fibonacci numbers
+ * main - printing first 98 fibonacci sequence
+ * Return: 0
  */
-void print_fibonacci(void)
-{
-unsigned long int a = 1, b = 2, next;
-int count;
-
-printf("%lu, %lu", a, b);
-
-for (count = 3; count <= 98; count++)
-{
-next = a + b;
-printf(", %lu", next);
-a = b;
-b = next;
-}
-
-printf("\n");
-}
-
 int main(void)
 {
-print_fibonacci();
-return (0);
+	unsigned long int current = 1;
+	unsigned long int next = 2;
+	int i;
+
+	printf("%lu", current);
+
+	for (i = 2; i < 98; i++)
+	{
+		printf(", ");
+		print_large_number(0, next);
+		next += current;
+		current = next - current;
+	}
+	printf("\n");
+
+	return (0);
 }
