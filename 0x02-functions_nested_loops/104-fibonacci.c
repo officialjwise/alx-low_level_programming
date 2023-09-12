a566a6fe8e3540a056eb6842c5ce4ca5d0bb60c0
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-12 21:42:10 +0000

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <gmp.h>
+
 /**
  * main - main block
  * Description: first 98 Fibonacci numbers, starting with 1 and 2
@@ -8,19 +10,37 @@
 int main(void)
 {
 	int i = 0;
-	unsigned long int a = 0, b = 1, next = 0;
+	mpz_t a, b, next;
+
+	// Initialize the mpz_t variables
+	mpz_init_set_ui(a, 0); // a = 0
+	mpz_init_set_ui(b, 1); // b = 1
+	mpz_init(next); // next = 0
 
 	while (i < 98)
 	{
-		next = a + b;
-		a = b;
-		b = next;
-		printf("%lu", next);
+		// next = a + b
+		mpz_add(next, a, b);
+
+		// a = b
+		mpz_set(a, b);
+
+		// b = next
+		mpz_set(b, next);
+
+		// Print next
+		gmp_printf("%Zd", next);
 
 		if (i < 97)
 			printf(", ");
 		i++;
 	}
 	putchar('\n');
+
+	// Clear the mpz_t variables
+	mpz_clear(a);
+	mpz_clear(b);
+	mpz_clear(next);
+
 	return (0);
 }
