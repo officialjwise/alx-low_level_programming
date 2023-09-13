71aa8a550aa64c1dcf6d8ad5343f49f68ec0ba1e
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-13 08:56:56 +0000

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,17 +11,6 @@ int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
 
-/*
-* This section of code is commented out to avoid an infinite loop.
-* Uncomment it to see the program enter an infinite loop.
-*/
-/*
-while (i < 10)
-{
-putchar(i);
-}
-*/
-
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
