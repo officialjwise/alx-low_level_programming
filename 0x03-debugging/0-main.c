4b26b479974eedd9d6cc785c25e8f3552db4334f
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-13 08:53:02 +0000

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point of the program to test positive_or_negative function
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point of the program to test positive_or_negative function
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    int i;
+int i;
 
-    i = 0;
-    positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-    return (0);
+return (0);
 }
