9742a37cf1cb33cb64692fa1f655ba5f2a1f916b
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-14 21:13:24 +0000

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 /**
- * _isupper - Check if a character is an uppercase letter.
- * @c: The character to be checked.
- *
- * Return: 1 if @c is an uppercase letter, 0 otherwise.
- */
+* _isupper - Check if a character is an uppercase letter.
+* @c: The character to be checked.
+*
+* Return: 1 if @c is an uppercase letter, 0 otherwise.
+*/
 #include "main.h"
 int _isupper(int c)
 {
-    return (c >= 'A' && c <= 'Z');
+return (c >= 'A' && c <= 'Z');
 }
