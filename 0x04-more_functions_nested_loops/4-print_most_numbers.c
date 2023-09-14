8d5878ec16340082932085357a534dd07e1aba65
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-09-14 16:22:26 -0700

    Print numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -23,29 +22,3 @@ void print_most_numbers(void)
 	}
 	_putchar('\n');
 }
-=======
-#include "main.h"
-
-/**
-*print_most_numbers - print numbers
-*
-*
-*/
-
-void print_most_numbers(void)
-{
-	int c = 48;
-
-	while (c <= 57)
-	{
-		if (c == 50 || c == 52)
-		{
-			c++;
-			continue;
-		}
-		_putchar(c);
-		c++;
-	}
-	_putchar('\n');
-}
->>>>>>> 6708832d3d79a80fdbbf07d15ef7ccafff2eec58
