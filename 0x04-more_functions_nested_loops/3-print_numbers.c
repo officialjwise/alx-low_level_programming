f5689fe57aa555b0efe812baa678b8ef5292b7bf
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-14 23:12:55 +0000

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints numbers 0 to 9 followed by a new line
+ * print_numbers - print 0-9
+ * return: void
  */
+
 void print_numbers(void)
 {
-    char digit;
+	char c;
 
-    for (digit = '0'; digit <= '9'; digit++)
-    {
-        _putchar(digit);
-    }
-
-    _putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
