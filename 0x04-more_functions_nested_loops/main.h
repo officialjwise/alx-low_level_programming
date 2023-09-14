9c1a9c610118d6728cabaa123b1d4b1a6400cb87
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-09-14 16:24:50 -0700

    Print numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_most_numbers(void);
 void print_numbers(void);
 #endif
