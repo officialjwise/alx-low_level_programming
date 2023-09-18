ffd1d78fd0f6577c71de53961e0d2312fe24fa1e
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-09-18 18:26:05 +0000

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
- * _strcpy - Copies a string including the null byte from src to dest.
- * @dest: Pointer to the destination buffer.
- * @src: Pointer to the source string.
- *
- * Return: Pointer to dest.
- */
+* _strcpy - Copies a string including the null byte from src to dest.
+* @dest: Pointer to the destination buffer.
+* @src: Pointer to the source string.
+*
+* Return: Pointer to dest.
+*/
 char *_strcpy(char *dest, char *src)
 {
 char *dest_start = dest;
@@ -22,6 +21,3 @@ src++;
 
 return dest_start;
 }
-=======
-
->>>>>>> 46b3e9e60f325b1914a619f57255d3822602e2e8
