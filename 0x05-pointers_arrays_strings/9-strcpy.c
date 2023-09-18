b2eef21ba3bb2a62456e4e6b09bbd1be8217d748
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-09-18 11:31:38 -0700

    String copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
-* _strcpy - Copies a string including the null byte from src to dest.
-* @dest: Pointer to the destination buffer.
-* @src: Pointer to the source string.
-*
-* Return: Pointer to dest.
-*/
+ * _strcpy - Copies a string including the null byte from src to dest.
+ * @dest: Pointer to the destination buffer.
+ * @src: Pointer to the source string.
+ *
+ * Return: Pointer to dest.
+ */
 char *_strcpy(char *dest, char *src)
 {
 char *dest_start = dest;
-
 while (*src != '\0')
 {
 *dest = *src;
 dest++;
 src++;
 }
+
 *dest = '\0';
-  
+
 return (dest_start);
 }
