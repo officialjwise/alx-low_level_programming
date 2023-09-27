c092fd24fde63100be8cfa8a873709bac8afbc90
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-09-27 16:19:02 -0700

    Palindrome solver

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,35 @@
 #include "main.h"
+
+/**
+ * check_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * @i: The start index.
+ * @len: The length of the string.
+ *
+ * Return: 1 if it's a palindrome, 0 if it's not.
+ */
+int check_palindrome(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_palindrome(s, i + 1, len - 1));
+}
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to calculate the length of.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
 /**
  * is_palindrome - Checks if a string is a palindrome.
  * @s: The string to be checked.
@@ -7,27 +38,7 @@
  */
 int is_palindrome(char *s)
 {
-	int length = strlen(s);
-	int i, j;
-
-	for (i = 0, j = length - 1; i < j; i++, j--)
-	{
-		if (s[i] != s[j])
-		{
-			return (0);
-		}
-	}
-
-	return (1);
-}
-
-int main(void)
-{
-	char str1[] = "racecar";
-	char str2[] = "hello";
-
-	printf("Is 'racecar' a palindrome? %d\n", is_palindrome(str1));
-	printf("Is 'hello' a palindrome? %d\n", is_palindrome(str2));
-
-	return (0);
+	if (*s == '\0')
+		return (1);
+	return (check_palindrome(s, 0, _strlen_recursion(s)));
 }
