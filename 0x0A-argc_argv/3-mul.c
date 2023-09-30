1b8c8bdfbd8f143c90720d41c2f334fb11b27afe
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-09-29 18:21:00 -0700

    Update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,10 +11,10 @@ int main(int argc, char *argv[])
 {
 	/* validate input */
 	if (argc != 3)
-		{
-			printf("Error\n");
-			return (1);
-		}
+	{
+		printf("Error\n");
+		return (1);
+	}
 	/* mulitply two arguments passed via cmd line */
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
