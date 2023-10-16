cea04828aaf3376f55b277e6a41b9d50017e9433
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-10-15 19:21:57 -0700

    Update multiplication of wo positive integers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,52 +4,51 @@
 #include <ctype.h>
 
 /**
- * isNumeric - Check if a string is composed of digits.
- * @str: The input string to check.
- * Return: 1 if composed of digits, 0 otherwise.
- */
-
+* isNumeric - Check if a string is composed of digits.
+* @str: The input string to check.
+* Return: 1 if composed of digits, 0 otherwise.
+*/
 int isNumeric(const char *str)
 {
-    while (*str)
-    {
-        if (!isdigit(*str))
-            return 0;
-        str++;
-    }
-    return 1;
+while (*str)
+{
+if (!isdigit(*str))
+return 0;
+str++;
+}
+return 1;
 }
 
 /**
- * multiply - Multiply two numbers.
- * @num1: The first number.
- * @num2: The second number.
- * Return: The result of the multiplication.
- */
+* multiply - Multiply two numbers.
+* @num1: The first number.
+* @num2: The second number.
+* Return: The result of the multiplication.
+*/
 int multiply(int num1, int num2)
 {
-    return num1 * num2;
+return num1 * num2;
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return 98;
-    }
+if (argc != 3)
+{
+printf("Error\n");
+return 98;
+}
 
-    if (!isNumeric(argv[1]) || !isNumeric(argv[2]))
-    {
-        printf("Error\n");
-        return 98;
-    }
+if (!isNumeric(argv[1]) || !isNumeric(argv[2]))
+{
+printf("Error\n");
+return 98;
+}
 
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2);
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2);
 
-    int result = multiply(num1, num2);
-    printf("%d\n", result);
+int result = multiply(num1, num2);
+printf("%d\n", result);
 
-    return 0;
+return 0;
 }
