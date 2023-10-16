1d93ad5f8767489c1005e4a0fee356a40061a511
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-10-15 21:38:30 -0700

    Update print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,113 +1,36 @@
 #include "variadic_functions.h"
-
-
+#include <stdio.h>
+#include <stdarg.h>
 /**
-* print_char - Prints a char.
-* @arg: A list of arguments pointing to
-* the character to be printed.
-*/
-void print_char(va_list arg)
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between strings.
+ * @n: The number of strings passed to the function.
+ * @...: A variable number of strings to be printed.
+ * Description: If separator is NULL, it is not printed.
+ *              If one of the strings if NULL, (nil) is printed instead.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-char letter;
+	va_list strings;
+	char *str;
+	unsigned int index;
 
-letter = va_arg(arg, int);
+	va_start(strings, n);
 
-printf("%c", letter);
-}
+	for (index = 0; index < n; index++)
+	{
+		str = va_arg(strings, char *);
 
-/**
-* print_int - Prints an int.
-* @arg: A list of arguments pointing to
-* the integer to be printed.
-*/
-void print_int(va_list arg)
-{
-int num;
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 
-num = va_arg(arg, int);
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
 
-printf("%d", num);
-}
+	printf("\n");
 
-
-/**
-* print_float - Prints a float.
-* @arg: A list of arguments pointing to
-* the float to be printed.
-*/
-void print_float(va_list arg)
-{
-float num;
-
-num = va_arg(arg, double);
-
-printf("%f", num);
-}
-
-
-
-/**
-* print_string - Prints a string.
-* @arg: A list of arguments pointing to
-* the string to be printed.
-*/
-void print_string(va_list arg)
-{
-char *str;
-
-str = va_arg(arg, char *);
-
-if (str == NULL)
-{
-printf("(nil)");
-return;
-}
-
-printf("%s", str);
-}
-
-/**
-*print_all - prints anything
-*@format: format of input
-*
-*Return: nothing
-*/
-void print_all(const char * const format, ...)
-{
-va_list args;
-
-int i = 0, j = 0;
-
-char *separator = "";
-
-printer_t funcs[] = {
-{"c", print_char},
-{"i", print_int},
-{"f", print_float},
-{"s", print_string}
-};
-
-va_start(args, format);
-
-while (format && (*(format + i)))
-{
-j = 0;
-
-while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
-j++;
-
-if (j < 4)
-{
-printf("%s", separator);
-funcs[j].print(args);
-separator = ", ";
-}
-
-i++;
-
-}
-
-printf("\n");
-
-va_end(args);
+	va_end(strings);
 }
