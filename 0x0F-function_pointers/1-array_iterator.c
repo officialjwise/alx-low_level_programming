40ad3f824c88a0ac7f33a59aed5498dbd81f413e
DANIEL AMOAKO KODUA <106404586+officialjwise@users.noreply.github.com>
2023-10-16 04:03:50 +0000

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,23 @@
-#include "functions_pointers.h"
+#include "function_pointers.h"
+
 /**
- * array_iterator - executes a function
- * @array: the array
- * @size: the size of an array
- * @action: a pointer to the function
- */
-void array_iterator(int *array, size_t size, void (*action)(int)
+*array_iterator - executes a function on each element of an array
+*@array: array to be targeted
+*@size: size of array
+*@action: function to be executed
+*
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i; /*match data type*/
 
-	if (array != NULL && action != NULL)
+if (array == NULL || action == NULL)
+return;
 
-	for (i = 0; i < size; i++)
+while (size-- > 0)
+{
+action(*array);
+array++;
+}
 
-	action(*(array + i));
 }
