fb310ebab57e5e843ea579106fa9539f9fca891f
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-10-28 09:07:39 +0000

    Pop list integer

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,25 +1,23 @@
 #include "lists.h"
-
 /**
-*pop_listint - deletes the head node of a linked list
-*@head: head of the list
-*
-*Return: the deleted node data
-*/
-
+ * pop_listint - deletes the head node and returns the head nodes data
+ * @head: linked list
+ * Return: deleted head node's data
+ */
 int pop_listint(listint_t **head)
 {
-listint_t *popped;
-int content;
+	int data;
+	listint_t *tmp;
 
-if (*head == NULL)
-return (0);
+	if (*head == NULL) /* account for no linked list */
+		return (0);
 
-popped = *head;
-content = popped->n;
-free(popped);
+	tmp = *head;
 
-*head = (*head)->next;
-return (content);
+	data = tmp->n; /* save data to return later */
 
+	*head = tmp->next; /* link head to next node */
+	free(tmp);
+
+	return (data);
 }
