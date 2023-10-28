867d0122cfd94de0e6c6f279a4a3d5db8920477b
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-10-28 09:38:35 +0000

    Print linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,3 +1,58 @@
+#include "lists.h"
+
+/**
+ * count_nodes_till_loop - count nodes to know how many unique nodes to print
+ * @head: pointer to head pointer of linked lists
+ * Return: number of unique nodes in list before a loop
+ */
+int count_nodes_till_loop(const listint_t *head)
+{
+	int count = 0;
+	const listint_t *turtle, *hare;
+
+	turtle = hare = head;
+
+	while (turtle != NULL && hare != NULL)
+	{
+		turtle = turtle->next;
+		hare = hare->next->next;
+		count++;
+
+		if (turtle == hare)
+		{
+			turtle = head;
+			while (turtle != hare)
+			{
+				turtle = turtle->next;
+				hare = hare->next;
+				count++;
+			}
+			return (count);
+		}
+	}
+	return (0);
+}
+/**
+ * loop - find if there's a loop in linked list
+ * @head: pointer to head pointer of linked list
+ * Return: 0 if no loop, 1 if loop
+ */
+int loop(const listint_t *head)
+{
+	const listint_t *turtle, *hare;
+
+	turtle = hare = head;
+
+	while (turtle != NULL && hare != NULL)
+	{
+		turtle = turtle->next;
+		hare = hare->next->next;
+
+		if (turtle == hare)
+			return (1);
+	}
+	return (0);
+}
 /**
  * print_listint_safe - prints list with addresses
  * @head: pointer to head pointer of linked list
